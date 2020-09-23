14672690c784cdc72849bb43c20ea4470c6c33d7
Ollie Blanks <ollieb@backslashgames.com>
2020-09-23 14:36:30 +0100

    Commit whole project

---
+++ Assets/Editor/FMODHelper.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using UnityEditor.Build.Reporting;
+
+public class FMODHelper : MonoBehaviour
+{
+    public static void PerformBuild ()
+    {
+        CopyToStreamingAssets();
+        
+        BuildPlayerOptions buildPlayerOptions = new BuildPlayerOptions();
+        buildPlayerOptions.scenes = new[] {"Assets/Scenes/SampleScene.unity"};
+        buildPlayerOptions.locationPathName = "OSXBuild";
+        buildPlayerOptions.target = BuildTarget.StandaloneOSX;
+        buildPlayerOptions.options = BuildOptions.None;
+
+        BuildReport report = BuildPipeline.BuildPlayer(buildPlayerOptions);
+        BuildSummary summary = report.summary;
+
+        if (summary.result == BuildResult.Succeeded)
+        {
+            Debug.Log("Build succeeded: " + summary.totalSize + " bytes");
+        }
+
+        if (summary.result == BuildResult.Failed)
+        {
+            Debug.Log("Build failed");
+        }
+    }
+    
+    public static void CopyToStreamingAssets()
+    {
+        FMODUnity.EventManager.UpdateCache();
+        FMODUnity.EventManager.CopyToStreamingAssets();
+    }
+}
